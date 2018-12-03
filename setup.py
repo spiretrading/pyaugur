774a7e3f15f06ef614c88862b28bac048a9b6a8a
Kamal <kamal@eidolonsystems.com>
2018-12-03 11:39:48 -0500

    Added starter code for basic API definitions and the setup.py script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+if __name__ == '__main__':
+  setup(name='pyaugur',
+        version='0.1',
+        description='Python bindings to the Augur Prediction Market.',
+        url='',
+        author='',
+        author_email='',
+        license='apache-2.0',
+        packages=['augur'],
+        install_requires=[])
